f8126a2f7e960f4501aac4867c06bca39bb20952
giova <giovannitopor@gmail.com>
2023-01-24 17:35:28 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+
+
+    public static void main(String...args) {
+    Programmer A = new Programmer();
+            A.name= "Luca";
+            A.age = 23;
+                A.isWearGlasses = true;
+
+                System.out.println(A - );
+
+
+    Programmer B = new Programmer();
+        B.name= "Milo";
+        B.age = 20;
+        B.isWearGlasses = false;
+
+
+    }
+}
---
+++ src/Programmer.java
@@ -0,0 +1,23 @@
+public class Programmer {
+    public String name;
+    public int age;
+    public Boolean isWearGlasses;
+}
+
+    public static void -drinkCoffee (String[] args){
+
+        System.out.println("Espresso is the secret!");
+        }
+
+
+public static void -printDetails (String[] args){
+
+        System.out.println("NameHere is a AgeHere-yo programmer");
+        }
+
+
+public static void -hasGlasses (String[] args){
+
+        System.out.println(" Is NameHere wearing glasses? followed by the value of the dedicated variable");
+        }
+
